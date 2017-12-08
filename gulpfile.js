16c40b719071e52bab41c64ac777e2f6e77c041a
Maeva NAP <maevanap.contact@gmail.com>
2017-12-08 10:18:49 +0100

    add basic gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,39 @@
-var gulp = require('gulp');
+/* REQUIRE */
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const pump = require('pump');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
 
-gulp.task('default', function() {
-  // place code for your default task here
+/* TOP LEVEL FUNCTIONS
+ *
+ * gulp.task - Define tasks
+ * gulp.src - Point to files to use
+ * gulp.dest - Point to folder to output
+ * gulp.watch
+ *
+ */
+
+// Copy HTML
+gulp.task('copyHtml', function() {
+  gulp.src('src/*.html')
+      .pipe(gulp.dest('app'));
 });
+
+// Compile SASS
+gulp.task('sass', function () {
+  return gulp.src('src/scss/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/css/style.css'));
+});
+
+// Minify + Concat JS files
+gulp.task('scripts', function() {
+  gulp.src('src/js/*.js')
+      .pipe(concat('main.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('app/js'));
+});
+
+// Default task
+gulp.task('default', ['copyHtml', 'sass', 'scripts']);
